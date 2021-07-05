ca4283d49e2b6f3b824c264d44a0dfa85735378d
Matias Araneda <maty.araneda@gmail.com>
2021-07-04 22:40:39 -0400

    ver 2

--- index.js
+++ index.js
@@ -60,8 +60,8 @@ app.use(
 app.use(express.json());
 
 app.get("/api/v1", (req, res) => {
-    res.send("<h2>Desde cloud beiby</h2>");
-    console.log("yeah it ran");
+    res.send("<h1>Cambiado para ver</h1>");
+    console.log("listo!!!");
 });
 
 //localhost:3000/api/v1/post/
